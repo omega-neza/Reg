80efcb0642711bdc9c4f25b55e8a72a3d06b2d9c
omega-neza <omeganeza@gmail.com>
2023-04-22 19:32:55 +0200

    small change

--- src/model/Course.java
+++ src/model/Course.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
- /*course model*/
+ 
 
 package model;
 
 /**
  *
- * @author omega
+ * 
  */
 public class Course {
     private String course_id;
